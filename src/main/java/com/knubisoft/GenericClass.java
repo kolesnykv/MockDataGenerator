2a5801421c9789a842879be23fc67b5f128e0aa8
Vladyslav Kolesnyk <v.kolesn1k369@gmail.com>
2022-09-09 00:10:40 +0300

    init commit

---
+++ src/main/java/com/knubisoft/GenericClass.java
@@ -0,0 +1,16 @@
+package com.knubisoft;
+
+import lombok.Getter;
+
+import java.lang.reflect.Type;
+
+@Getter
+public abstract class GenericClass<T> {
+    private final T t;
+    private final Type type;
+
+    public GenericClass(T t) {
+        this.t = t;
+        this.type = this.getClass().getGenericSuperclass();;
+    }
+}
---
+++ src/main/java/com/knubisoft/X.java
@@ -0,0 +1,19 @@
+package com.knubisoft;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+
+@Getter
+@Setter
+@ToString
+public class X {
+    private String name;
+    private Boolean flag;
+    private Integer integer;
+    private List<String> chacha;
+
+}
